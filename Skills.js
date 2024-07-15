a64ff66357d09f551727c13bcd396d0a13b4d775
Scotty <scottmtinez@gmail.com>
2024-07-15 13:40:17 -0500

    Create Skills.js

---
+++ Skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Skills.css';
+
+function Skills() {
+  return (
+    <div className='skills-container' id='skills'>
+      <h1 className='skills-title'>Skills</h1>
+      <div className='skills-list'>
+        test
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
