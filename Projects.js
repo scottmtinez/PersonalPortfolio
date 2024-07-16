a9cf212124e3577411c402771f77d6c256b1e809
Scotty <scottmtinez@gmail.com>
2024-07-16 17:36:34 -0500

    Create Projects.js

---
+++ Projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+  return (
+    <div className='projects' id='projects'>
+      <h1 className='projects-title'>Projects</h1>
+      <div className='projects-container'>
+        <div className='projects-box'>1</div>
+        <div className='projects-box'>2</div>
+        <div className='projects-box'>3</div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
