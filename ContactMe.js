4c15f1bf74a1528e31749ded29a8814655a6e01f
Scotty <scottmtinez@gmail.com>
2024-07-24 20:07:41 -0500

    Update ContactMe.js

--- ContactMe.js
+++ ContactMe.js
@@ -6,10 +6,25 @@ function ContactMe() {
     <div className='contactMe' id='contactMe'>
       <h1 className='contactMe-title'>Contact Me</h1>
       <form className='contactMe-form'>
-        <input type='text'></input>
-        <input type='text'></input>
-        <input type='text'></input>
-        <input type='submit'></input> 
+
+        <div className='form-group'>
+          <input type='text' className='name' id='name' placeholder='Name' required></input>
+        </div>
+
+        <div className='form-group'>
+          <input type='text' className='company' id='company' placeholder='Company'></input>
+        </div>
+
+        <div className='form-group'>
+          <input type='email' className='email' id='email' placeholder='Email' required></input>
+        </div>
+
+        <div className='form-group'>
+          <textarea className='message' id='message' placeholder='Message' required></textarea>
+        </div>
+
+        <input type='submit' className='message' value='Send Message'></input>
+      
       </form>
     </div>
   );
