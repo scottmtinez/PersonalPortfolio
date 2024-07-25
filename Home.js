0da7bd449e533b7e3db57a6730868953269c2c1a
Scotty <scottmtinez@gmail.com>
2024-07-24 19:58:40 -0500

    Update Home.js

--- Home.js
+++ Home.js
@@ -3,8 +3,7 @@ import './Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import myself from './myself.jpg';
-import menu from './Menu';
-import Menu from './Menu';
+import resume from './Resume - Scott Martinez (1).pdf';
 
 function Home(){
     return (
@@ -21,9 +20,7 @@ function Home(){
                         <a href='https://www.instagram.com/scottmtinez/'><i class="bi bi-instagram" id='home-instagram'></i></a>
                         <a href='https://www.linkedin.com/in/scott-martinez-290a40249?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bo4aVek64QwiEFlRQRqQHow%3D%3D'><i class="bi bi-linkedin" id='home-linkedin'></i></a>
                         <a href='https://github.com/scottmtinez'><i class="bi bi-github" id='home-github'></i></a>
-                        <a href='../Resume.pdf' ><i class="bi bi-download" id='home-resume'></i></a> {/* NOTE: Needs to be fixed*/}
-
-
+                        <a href={resume} ><i class="bi bi-download" id='home-resume'></i></a> {/* NOTE: Needs to be fixed*/}
                     </div>
                 </div>
             </div>
