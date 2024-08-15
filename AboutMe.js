d81e3001765d94ac2c575dbbabfdaa9b1ee0e382
Scott Martínez <scottmtinez@gmail.com>
2024-08-15 15:39:52 -0500

    Update AboutMe.js

--- AboutMe.js
+++ AboutMe.js
@@ -9,19 +9,19 @@ function AboutMe() {
           <h1 className='aboutMe-title fade-in'>About Me</h1>
           <p className='aboutMe-desc'>
             I am Scott Martinez, a recent graduate with a Bachelor of Science in Computer Science from the University of Wisconsin - 
-            Whitewater, specializing in Full-Stack Development, Database Manangement & AI Development. I posses a strong foundate in
-            Html, CSS, PHP, Node.js, React.js, MySQL & I am consistently staying up to date with the lastest frameworks and technologies 
+            Whitewater, specializing in Full-Stack Development, Database Manangement & AI Development. I possess a strong foundation in
+            HTML, CSS, PHP, Node.js, React.js, MySQL & I am consistently staying up to date with the latest frameworks and technologies 
             to improve my programming skills. <br/><br/>
 
-            Throughout my time at Whitewater, I have led a small team of developers to design and build a social media like web
-            application and present it in front of our client. I built differet websites and web applications that use softwares like MySQL, 
-            Firebase, and MongoDB. I created a security system using Linux and Python, to check to see if a file has been tampered with.
+            Throughout my time at Whitewater, I have led a small team of developers to design and build a social media-like web
+            application and present it to our client. I built different websites and web applications that use software like MySQL, 
+            Firebase, and MongoDB. I created a security system using Linux and Python, to check if a file has been tampered with.
             <br/><br/>
 
-            After graduating from Whitewater, I self taught myself how to create an AI system that uses React.js to recommend 
-            clothing and items for a vacation that user may be going on in the future. The AI system then collects the users
-            vacation information that gets the weather data of that location and then the system is able to recommend the correct
-            clothing and items for the vacation. 
+            After graduating from Whitewater, I taught myself how to create an AI system that uses React.js to recommend 
+            clothing and items for a vacation that users may be going on in the future. The AI system then collects the user's
+            vacation information and then gets the weather data of that location and then the system can recommend the correct
+            clothing and items for the vacation.  
             <br/><br/>
 
             These experiences have honed my technical skills and enhanced my ability to communicate complex ideas effectively to 
